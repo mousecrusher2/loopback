5fb7fb1ca0b27081442790d6b83fb781b7f23be1
mousecrusher2 <95418154+mousecrusher2@users.noreply.github.com>
2026-07-07 03:09:55 +0900

    refactor: streamline USB configuration and handler initialization

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use embassy_executor::Spawner;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::{Peri, peripherals};
 use embassy_time::Timer;
-use embassy_usb::UsbDevice;
-use embassy_usb::driver::Driver;
-use embassy_usb::{Builder, Config};
+use embassy_usb as usb;
 use panic_halt as _;
 use pico2_uac1_loopback::audio::AudioState;
 use pico2_uac1_loopback::control::AudioControlHandler;
@@ -16,6 +14,7 @@ use pico2_uac1_loopback::diag;
 use pico2_uac1_loopback::irq::{UsbDriver, usb_driver};
 use pico2_uac1_loopback::tasks::{PacketQueue, in_task, out_task};
 use static_cell::StaticCell;
+use usb::driver::Driver;
 
 static AUDIO_STATE: AudioState = AudioState::new();
 static PACKETS: PacketQueue = PacketQueue::new();
@@ -25,25 +24,34 @@ async fn main(spawner: Spawner) {
     let p = embassy_rp::init(embassy_rp::config::Config::default());
     let driver = usb_driver(p.USB);
 
-    static CONFIG_DESCRIPTOR: StaticCell<[u8; 1024]> = StaticCell::new();
-    static BOS_DESCRIPTOR: StaticCell<[u8; 32]> = StaticCell::new();
-    static MSOS_DESCRIPTOR: StaticCell<[u8; 32]> = StaticCell::new();
-    static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
-    static HANDLER: StaticCell<AudioControlHandler> = StaticCell::new();
+    let config_descriptor = {
+        static CELL: StaticCell<[u8; 1024]> = StaticCell::new();
+        CELL.init([0; 1024])
+    };
+    let bos_descriptor = {
+        static CELL: StaticCell<[u8; 32]> = StaticCell::new();
+        CELL.init([0; 32])
+    };
+    let msos_descriptor = {
+        static CELL: StaticCell<[u8; 32]> = StaticCell::new();
+        CELL.init([0; 32])
+    };
+    let control_buf = {
+        static CELL: StaticCell<[u8; 64]> = StaticCell::new();
+        CELL.init([0; 64])
+    };
 
-    let config_descriptor = CONFIG_DESCRIPTOR.init([0; 1024]);
-    let bos_descriptor = BOS_DESCRIPTOR.init([0; 32]);
-    let msos_descriptor = MSOS_DESCRIPTOR.init([0; 32]);
-    let control_buf = CONTROL_BUF.init([0; 64]);
+    let config = {
+        let mut config = usb::Config::new(0xcafe, 0x4001);
+        config.manufacturer = Some("Embassy");
+        config.product = Some("Pico 2 UAC1 Loopback");
+        config.serial_number = Some("pico2-loopback-0005");
+        config.max_packet_size_0 = 64;
+        config.max_power = 100;
+        config
+    };
 
-    let mut config = Config::new(0xcafe, 0x4001);
-    config.manufacturer = Some("Embassy");
-    config.product = Some("Pico 2 UAC1 Loopback");
-    config.serial_number = Some("pico2-loopback-0005");
-    config.max_packet_size_0 = 64;
-    config.max_power = 100;
-
-    let mut builder = Builder::new(
+    let mut builder = usb::Builder::new(
         driver,
         config,
         config_descriptor,
@@ -53,14 +61,17 @@ async fn main(spawner: Spawner) {
     );
 
     let endpoints = build_audio_function(&mut builder);
-    let handler = HANDLER.init(AudioControlHandler::new(
-        &AUDIO_STATE,
-        &PACKETS,
-        endpoints.out_streaming_if,
-        endpoints.in_streaming_if,
-        endpoints.out_ep_addrs,
-        endpoints.in_ep_addrs,
-    ));
+    let handler = {
+        static CELL: StaticCell<AudioControlHandler> = StaticCell::new();
+        CELL.init(AudioControlHandler::new(
+            &AUDIO_STATE,
+            &PACKETS,
+            endpoints.out_streaming_if,
+            endpoints.in_streaming_if,
+            endpoints.out_ep_addrs,
+            endpoints.in_ep_addrs,
+        ))
+    };
     builder.handler(handler);
 
     spawner.spawn(usb_task(builder.build()).expect("USB task pool exhausted"));
@@ -74,8 +85,8 @@ async fn main(spawner: Spawner) {
 }
 
 #[embassy_executor::task]
-async fn usb_task(mut usb: UsbDevice<'static, UsbDriver>) {
-    usb.run().await;
+async fn usb_task(mut usb_device: usb::UsbDevice<'static, UsbDriver>) {
+    usb_device.run().await;
 }
 
 #[embassy_executor::task(pool_size = 3)]
